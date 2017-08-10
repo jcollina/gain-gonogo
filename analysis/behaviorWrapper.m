a1d0e051d6a09e951684f744db776786ac6f80f0
chris <chris.angeloni@gmail.com>
2017-08-10 09:24:31 -0400

    analysis update

--- analysis/behaviorWrapper.m
+++ analysis/behaviorWrapper.m
@@ -1,5 +1,5 @@
-mouseList = {'CA049'};%{'CA046','CA047','CA048','CA049'};
-mouseTask = 1%[0 1 0 1];
+mouseList = {'CA046','CA047','CA048','CA049'};
+mouseTask = [0 1 0 1];
 for i = 1:length(mouseList)
     analyzeBehavior(mouseList{i},mouseTask(i));
 end
