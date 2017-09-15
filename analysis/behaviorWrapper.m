a647d77f7bf43302c00feab400414a50b2c5eb58
chris <chris.angeloni@gmail.com>
2017-09-15 13:31:51 -0400

    analysis update

--- analysis/behaviorWrapper.m
+++ analysis/behaviorWrapper.m
@@ -1,7 +1,22 @@
-mouseList = {'CA046','CA047','CA048','CA049'};
-mouseTask = [0 1 0 1];
-%mouseList = {'CA047'};
-%mouseTask = [1];
+function behaviorWrapper(mouseList,mouseTask)
+
+if ~exist('mouseList','var')
+    mouseList = {'CA046','CA047','CA048','CA049'};
+end
+if ~exist('mouseTask','var')
+    for i = 1:length(mouseList)
+        if strcmp(mouseList{i},'CA046')
+            mouseTask(i) = 0;
+        elseif strcmp(mouseList{i},'CA047')
+            mouseTask(i) = 1;
+        elseif strcmp(mouseList{i},'CA048')
+            mouseTask(i) = 0;
+        elseif strcmp(mouseList{i},'CA049')
+            mouseTask(i) = 1;
+        end
+        %mouseTask = [0 1 0 1];
+    end
+end
 
 for i = 1:length(mouseList)
     analyzeBehavior(mouseList{i},mouseTask(i));
