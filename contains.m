80770e1ed0439413cc27f49ecf7e7f2d7cc3fad1
Behavior4 <geffenlab@gmail.com>
2018-01-12 11:08:07 -0500

    data update and contains fix

--- contains.m
+++ contains.m
@@ -1,3 +1,17 @@
-function status = contains(a,b)
+function tf = contains(s,pattern,varargin)
 
-status = ~isempty(strfind(a,b));
+% for one string
+narginchk(2, inf);
+
+try
+    stringS = string(s);
+    
+    if nargin == 2
+        tf = stringS.contains(pattern);
+    else
+        tf = stringS.contains(pattern, varargin{:});
+    end
+    
+catch E
+    throw(E)
+end
