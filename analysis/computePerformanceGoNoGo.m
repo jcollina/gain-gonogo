d352713e24cfb35f6c986d106fa3e56680996212
chris <chris.angeloni@gmail.com>
2018-04-23 10:52:23 -0400

    analysis update

--- analysis/computePerformanceGoNoGo.m
+++ analysis/computePerformanceGoNoGo.m
@@ -1,4 +1,4 @@
-function [hr,fa,dp,pc,goodIdx] = computePerformanceGoNoGo(resp,ttype,start,crit)
+function [hr,fa,dp,pc,goodIdx] = computePerformanceGoNoGo(resp,ttype,start,crit,respThresh)
     
 %% function [hr,fa,dp,pc] = computePerformance(resp,ttype,truncate)
 % 
@@ -10,10 +10,13 @@ function [hr,fa,dp,pc,goodIdx] = computePerformanceGoNoGo(resp,ttype,start,crit)
 % the end of the run, by counting the number of responses from the
 % end of the run and stopping when it hits a certain criterion lick
 % count provided by [crit]
-    
+
 % Truncate based on responses to get rid of the end where mice
 % doesn't lick
-if nargin == 4
+if exist('crit','var') & ~isempty(crit)
+    if ~exist('start','var')|isempty(start)
+        start = 20;
+    end
     startv = start;
     count_back = sum(resp) - cumsum(resp);
     endnum = find(count_back == crit);
@@ -25,12 +28,18 @@ if nargin == 4
     
     goodIdx = zeros(1,length(resp));
     goodIdx(startv:endv) = 1;
-    
-    resp = resp(startv:endv);
-    ttype = ttype(startv:endv);
 else
     goodIdx = ones(1,length(resp));
 end
+
+% remove periods where there were no licks
+if exist('respThresh','var')
+    respMean = movmean(resp,15);
+    goodIdx = respMean >= respThresh;
+end
+
+resp = resp(goodIdx);
+ttype = ttype(goodIdx);
 
 ttype = ttype>0;
 if any(size(resp) ~= size(ttype))
@@ -44,16 +53,16 @@ fa = mean(resp(ttype==0));
 hr1 = hr;
 fa1 = fa;
 if hr1 == 1
-    hr1 = .999;
+    hr1 = .99;
 end
 if hr1 == 0
-    hr1 = .001;
+    hr1 = .01;
 end
 if fa1 == 0
-    fa1 = .001;
+    fa1 = .01;
 end
 if fa1 == 1
-    fa1 = .999;
+    fa1 = .99;
 end
 
 dp = norminv(hr1) - norminv(fa1);
