2930a4b06a9d4f213488bb78434431a9537e9926
Sandman-stim <geffenlab@gmail.com>
2018-05-12 14:15:15 -0400

    data update

--- _arduino_code/debouncePrime_inv/debouncePrime_inv.ino
+++ _arduino_code/debouncePrime_inv/debouncePrime_inv.ino
@@ -14,7 +14,7 @@ int lastButtonState = LOW;   // the previous reading from the input pin
 // the following variables are unsigned long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 1;    // the debounce time; increase if the output flickers
+unsigned long debounceDelay = 10;    // the debounce time; increase if the output flickers
 
 void setup() {
   pinMode(buttonPin, INPUT);
