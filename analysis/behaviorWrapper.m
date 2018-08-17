c41f42ad931b673139de15e9a58ece9077531542
Sandman-stim <geffenlab@gmail.com>
2018-08-17 09:17:25 -0400

    analysis update

--- analysis/behaviorWrapper.m
+++ analysis/behaviorWrapper.m
@@ -4,7 +4,7 @@ function behaviorWrapper(mouseList)
 addpath(genpath('../Palamedes/'));
 
 if ~exist('mouseList','var')
-    mouseList = {'CA046','CA047','CA048','CA049','CA051','CA052','CA055','CA061','CA072'};
+    mouseList = {'CA046','CA047','CA048','CA049','CA051','CA052','CA055','CA061','CA072','CA075'};
 end
 
 for i = 1:length(mouseList)
