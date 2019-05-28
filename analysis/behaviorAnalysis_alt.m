c2c4c2606ce9bfa0f6f33535a41575986be83525
angelonc <chris.angeloni@gmail.com>
2019-05-27 22:40:37 -0400

    analysis update
    added more mice, compared when removing trials where the mouse licked early. didn't effect the results too much. also updated threshold analysis

--- analysis/behaviorAnalysis_alt.m
+++ analysis/behaviorAnalysis_alt.m
@@ -17,6 +17,11 @@ for i = 1:length(mouseList)
     
     dpwit(:,:,i) = dat_w(i).offset.meanDPthresh;
     dpwitout(:,:,i) = dat_wo(i).offset.meanDPthresh;
+    
+    thresh(1,i) = mean(dat_w(i).offset.snr(1,dat_w(i).offset.contrast ...
+                                           == 1));
+    thresh(2,i) = mean(dat_w(i).offset.snr(1,dat_w(i).offset.contrast ...
+                                           == 2));
 end
 
 % replace missing data with nans
@@ -24,29 +29,87 @@ dpwit(dpwit == 0) = nan;
 dpwitout(dpwitout == 0) = nan;
 
 taskStr = {'LoHi','HiLo'};
-lineColor = [1 0 0; 0 0 1];
+lineColor = [0 0 1;1 0 0];
 
 
-clf; hold on;
+f1 = figure(1); clf; hold on;
 e = errorbar(repmat(dat_w(1).offset.offset,2,1)',...
          flipud(nanmean(dpwit,3))',...
          flipud(nanstd(dpwit,0,3) ./ sqrt(length(dat_w)-1))',...
          '--');
-e(1).Color = lineColor(2,:); e(2).Color = lineColor(1,:);
+e(1).Color = lineColor(1,:); e(2).Color = lineColor(2,:);
 e = errorbar(repmat(dat_w(1).offset.offset,2,1)',...
          flipud(nanmean(dpwitout,3))',...
          flipud(nanstd(dpwitout,0,3) ./ sqrt(length(dat_w)-1))',...
          'LineWidth',1.5)
-e(1).Color = lineColor(2,:); e(2).Color = lineColor(1,:);
+e(1).Color = lineColor(1,:); e(2).Color = lineColor(2,:);
 xlim([0 1.05]);
 legend('LC - licks','HC - licks','LC - nolicks','HC - nolicks',...
        'Location','nw')
-set(gca,'XTick',dat(1).offset.offset);
+set(gca,'XTick',dat_w(1).offset.offset);
 xlabel('Time (s)')
 ylabel('d''')
+plotPrefs;
 
+fn = './_dp_over_time_alt_analysis.pdf';
+saveFigPDF(f1,[500 300],fn);
 
+%% original analysis
 
+offsets = dat_w(1).offset.offset;
+
+% for each contrast
+for i = 1:2
+    % for the first two times
+    for j = 1:2
+        [~,p(i,j)] = ttest(squeeze(dpwit(i,1,:)),...
+                           squeeze(dpwit(i,j+1,:)));
+    end
+end
+
+f2 = figure(2); clf; hold on;
+e = errorbar(repmat(dat_w(1).offset.offset,2,1)',...
+         flipud(nanmean(dpwit,3))',...
+         flipud(nanstd(dpwit,0,3) ./ sqrt(length(dat_w)-1))',...
+         'LineWidth',1.5)
+e(1).Color = lineColor(1,:); e(2).Color = lineColor(2,:);
+xlim([0 1.05]);
+
+set(gca,'XTick',offsets);
+xlabel('Time (s)')
+ylabel('d''')
+plotPrefs;
+
+yv = flipud([2.5 2.625; 2.75 2.875]);
+lc = flipud(lineColor);
+% for each contrast
+for i = 1:2
+    % for the first two times
+    for j = 1:2
+        [~,p(i,j)] = ttest(squeeze(dpwit(i,1,:)),...
+                           squeeze(dpwit(i,j+1,:)));
+        plot([offsets(1) offsets(j+1)]-.02,[yv(i,j) yv(i,j)],...
+             'Color',lc(i,:),'LineWidth',3)
+        if p(i,j) > .1
+            str = 'NS';
+        elseif p(i,j) > .05 & p(i,j) < .1
+            str = sprintf('p = %04.3f',p(i,j));
+        elseif p(i,j) < .05 & p(i,j) > .01
+            str = '*';
+        elseif p(i,j) < .01 & p(i,j) > .001
+            str = '**';
+        elseif p(i,j) < .001
+            str = '***';
+        end
+        text(offsets(j+1),yv(i,j),str);
+    end
+end
+legend(e,'High to Low','Low to High',...
+       'Location','se')
+title(sprintf('n = %d',size(dpwit,3)))
+
+fn = './_dp_over_time_analysis.pdf';
+saveFigPDF(f2,[500 300],fn);
 
 
 function [dat] = behaviorAnalysis_earlyLicks(ID,includeEarlyLicks)
